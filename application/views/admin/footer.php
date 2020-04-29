5a035cac85faa4d24c70c105d841bb618647574e
unknown <ahmadbadruz17@gmail.com>
2020-04-29 10:00:23 +0700

    first commit

---
+++ application/views/admin/footer.php
@@ -0,0 +1,6 @@
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2020-2021 <a href="http://adminlte.io">Ahmad Badru Zaman | 181240000805</a></strong>
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b>2014
+    </div>
+  </footer>
